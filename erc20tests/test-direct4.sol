55d4c12c999982a21073a0d55a3cf7b9a91e31ec
Tristan Hermanns <s8trherm@stud.uni-saarland.de>
2021-10-05 02:59:21 +0200

    fixed todo

---
+++ erc20tests/test-direct4.sol
@@ -0,0 +1,61 @@
+pragma solidity ^0.4.0;
+
+contract VulnerableToken{
+
+    uint public _totalSupply = 10000;
+    mapping(address => uint256) balances;
+    mapping(address => mapping (address => uint256)) allowed;
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+
+    function _totalSupply() public constant returns (uint){
+        return _totalSupply;
+    }
+
+    function balanceOf(address who) public constant returns (uint){
+        return balances[who];
+    }
+
+    function transfer(address receiver, uint numTokens) public returns (bool) {
+        require(numTokens <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender] - numTokens;
+        balances[receiver] = balances[receiver] + numTokens;
+        emit Transfer(msg.sender, receiver, numTokens);
+        return true;
+    }
+
+    function approve(address delegate, uint numTokens) public returns (bool) {
+        allowed[msg.sender][delegate] = numTokens;
+        emit Approval(msg.sender, delegate, numTokens);
+        return true;
+    }
+
+    function allowance(address owner, address delegate) public view returns (uint) {
+        return allowed[owner][delegate];
+    }
+
+    function transferFrom(address owner, address buyer, uint numTokens) public returns (bool) {
+        require(numTokens <= balances[owner]);
+        require(numTokens <= allowed[owner][msg.sender]);
+        balances[owner] = balances[owner] - numTokens;
+        allowed[owner][msg.sender] = allowed[owner][msg.sender] - numTokens;
+        balances[buyer] = balances[buyer] + numTokens;
+        emit Transfer(owner, buyer, numTokens);
+        return true;
+    }
+
+    function batchTransfer(address[] _receivers, uint _value) public returns (bool) {
+        uint cnt = _receivers.length;
+        uint amount = uint(cnt) * _value;
+        require(cnt > 0 && cnt <= 20);
+        require(_value > 0 && balances[msg.sender] >= amount);
+
+        balances[msg.sender] = balances[msg.sender] - amount;
+        for (uint i = 0; i < cnt; i++) {
+            balances[_receivers[i]] = balances[_receivers[i]] + _value;
+            emit Transfer(msg.sender, _receivers[i], _value);
+        }
+        return true;
+    }
+}
